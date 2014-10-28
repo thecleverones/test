d2ad192ae2989135a63df630f1cc75156844d431
fatRyan <ryan@webcafe.io>
2014-10-28 12:46:26 +0000

    ajax js file
    Hey

---
+++ theCleverOnes.js
@@ -0,0 +1,67 @@
+function pageState(state){
+
+	//SPLIT ELEMENTS - (MODULE|STATE)
+	var elements  = state.split('|');
+	var module    = elements[0];
+	var subModule = elements[1];
+	var primaryKeyID = '';
+	var type = '';
+	var path = window.location.pathname;
+
+	$.ajax({
+	  url: "modules/pageState.php",
+	  type:"get",
+	  data: {
+						ajaxRequest: "pageState",
+						module: module,
+						subModule: subModule,
+						primaryKeyID: primaryKeyID
+					}
+
+		success: function(response) {
+
+			pageModule(response);
+
+	  },
+
+	  error: function(xhr) {
+
+			logError(xhr);
+
+
+	  }
+
+	});
+
+}
+
+function pageModule(){
+
+
+
+
+}
+
+function logError(error){
+
+	$.ajax({
+		url: "modules/error/logError.php",
+		type:"get",
+		data: {
+						ajaxRequest: "logError",
+						error: encodeURIComponent(error)
+					}
+
+		success: function(response) {
+
+			pageModule(response);
+
+		},
+	  error: function(xhr,error) {
+
+			alert("An error occured logging there error, this is the 'logError' function error: '" + xhr + "'. This error originated from the following error: '" + error + "'.");
+
+		}
+
+
+}
